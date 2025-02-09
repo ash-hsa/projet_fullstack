9037be63ea3813e239af5b6dcf1f8641c5cefb42
ash-hsa <darkazant9@gmail.com>
2025-02-09 17:06:41 +0100

    remise en forme du build + debut auth

---
+++ backend/app/src/main/java/org/example/rest/LoginController.java
@@ -0,0 +1,22 @@
+package org.example.rest;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("login")
+public class LoginController {
+    
+    @GetMapping()
+    public ResponseEntity<Void> login() {
+        return ResponseEntity.ok().build();
+    }
+    
+    
+}

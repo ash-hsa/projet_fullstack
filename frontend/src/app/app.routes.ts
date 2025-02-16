7dae83551e048675728417b36320a24501a10ba3
sonny <sonny.lahmadi1@etu.univ-lorraine.fr>
2025-02-16 22:49:53 +0100

    bouton pour accéder aux pages admin

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { VaccinationCenterListComponent } from './vaccination-center-list/vaccin
 import { VaccinationCenterComponent } from './vaccination-center/vaccination-center.component';
 import { LoginComponent } from './login/login.component';
 import { AccueilComponent } from './accueil/accueil.component';
-
+import { AccueilAdminComponent } from './accueil-admin/accueil-admin.component';
 
 
 export const routes: Routes = [
@@ -11,6 +11,7 @@ export const routes: Routes = [
     {path: "centers", component: VaccinationCenterListComponent},
     {path : "centers/detail/:id", component: VaccinationCenterComponent},
     {path: "accueil", component: AccueilComponent},
+    {path: "accueil_admin", component: AccueilAdminComponent},
     {path : "", redirectTo: "/login", pathMatch:"full"},
     
 ];

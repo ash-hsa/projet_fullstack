98e4605964e7af94f0150d0481138900584fe9ea
sonny <sonny.lahmadi1@etu.univ-lorraine.fr>
2025-02-19 14:14:14 +0100

    création page pour modifier le profil du patient

---
+++ frontend/src/app/modifier-profil-patient/modifier-profil-patient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modifier-profil-patient',
+  standalone: true,
+  imports: [],
+  templateUrl: './modifier-profil-patient.component.html',
+  styleUrl: './modifier-profil-patient.component.scss'
+})
+export class ModifierProfilPatientComponent {
+
+}

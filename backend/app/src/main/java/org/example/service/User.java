a0db00dd9f8322f7eeeb510fd140ac121d24f163
sonny <sonny.lahmadi1@etu.univ-lorraine.fr>
2025-02-20 20:10:43 +0100

    Afficher les médecins d'un cenre selon l'admin : temporaire car pas d'authentification donc on fore un admin pour les tests

--- backend/app/src/main/java/org/example/service/User.java
+++ backend/app/src/main/java/org/example/service/User.java
@@ -45,6 +45,7 @@ public class User {
         this.isSAdmin = isSAdmin;
         this.addressId = addressId;
     }
+    
 
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }

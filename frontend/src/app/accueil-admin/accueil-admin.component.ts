d0414fd122a0d323972de32709affda2cdc1fec1
Hugo.E <chonps.hugo@gmail.com>
2025-02-25 18:05:27 +0100

    Ajout_bouton_de_retour

--- frontend/src/app/accueil-admin/accueil-admin.component.ts
+++ frontend/src/app/accueil-admin/accueil-admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-accueil-admin',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './accueil-admin.component.html',
   styleUrl: './accueil-admin.component.scss'
 })
--- frontend/src/app/accueil-super-admin/accueil-super-admin.component.ts
+++ frontend/src/app/accueil-super-admin/accueil-super-admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-accueil-super-admin',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './accueil-super-admin.component.html',
   styleUrl: './accueil-super-admin.component.scss'
 })

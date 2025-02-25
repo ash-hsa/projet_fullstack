21d55ec5ed45e289f005de94fe10e62899f7d890
Hugo.E <chonps.hugo@gmail.com>
2025-02-25 18:40:02 +0100

    Meilleur_toolbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { VaccinationCenterComponent } from './vaccination-center/vaccination-center.component';
 import { VaccinationCenterListComponent } from "./vaccination-center-list/vaccination-center-list.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -7,7 +8,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbarModule, RouterModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
